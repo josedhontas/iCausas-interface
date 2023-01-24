e88b596e977090a2194e413f7771fd296db417f3
josedhonatas <dhonatas@academico.ufs.br>
2023-01-24 11:02:44 -0300

    primeiro envio

---
+++ icausas/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Barramenu from './Components/barramenu';
+
+function App() {
+  return (
+    <Barramenu> </Barramenu>
+    );
+}
+
+export default App;
