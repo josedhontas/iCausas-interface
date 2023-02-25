3ec8180993d2b6187a72f294ea0c16772db7831f
josedhonatas <dhonatas@academico.ufs.br>
2023-02-25 17:01:52 -0300

    enviologin

--- icausas/src/Pages/login.js
+++ icausas/src/Pages/login.js
@@ -1,43 +1,67 @@
-import { Avatar, Grid, Link, Paper, TextField, Typography } from '@material-ui/core'
-import React from 'react'
-import Checkbox from '@mui/material/Checkbox';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Button from '@mui/material/Button';
-import LockIcon from '@mui/icons-material/Lock';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: theme.spacing(2),
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)',
+    padding: theme.spacing(4),
+    backgroundColor: '#f5f5f5',
+    maxWidth: 360,
+    margin: 'auto',
+    height: '80vh', // altura reduzida
+  },
+  title: {
+    fontWeight: 700,
+    marginBottom: theme.spacing(3),
+  },
+  subtitle: {
+    color: '#9e9e9e',
+    marginBottom: theme.spacing(2),
+  },
+  button: {
+    backgroundColor: '#212121',
+    color: 'white',
+    marginTop: theme.spacing(3),
+    '&:hover': {
+      backgroundColor: '#424242',
+    },
+  },
+}));
 
+function Login() {
+  const classes = useStyles();
 
+  const handleGoogleLogin = () => {
+    // Adicione aqui a lógica para iniciar o processo de autenticação do Google
+  };
 
-export default function Login() {
-  const paperStyle = { padding: 20, height: '60vh', width: 300, margin: "70px auto"}
-  const btnstyle={margin:'8px 0'}
   return (
-    <Grid>
-      <Paper elevation={10} style={paperStyle}>
-        <Grid align='center'>             <Avatar><LockIcon/></Avatar>
-          <h2>iCausas</h2>
-        </Grid>
-        <TextField label='Email' placeholder='Insira seu email' fullWidth required></TextField>
-        <TextField label='Senha' placeholder='Insira sua senha' type='password' fullWidth required></TextField>
-        <FormGroup>
-          <FormControlLabel control={<Checkbox />} label="Manter conectado" />
-          <Button type='submit' variant='contained' style={btnstyle}>
-            Entrar
-          </Button>
-        </FormGroup>
-        <Typography align='left'>
-          <Link href='#'>
-            Esqueceu a senha?
-          </Link>
-        </Typography>
-        <Typography align='left'>
-          Novo por aqui? 
-          <Link href='#'>
-            Criar conta
-          </Link>
-        </Typography>
-      </Paper>
-    </Grid>
-  )
+    <Box className={classes.container}>
+      <Typography variant="h4" className={classes.title}>
+        iCausas
+      </Typography>
+      <Typography variant="subtitle1" className={classes.subtitle}>
+        Faça login com sua conta Google
+      </Typography>
+      <Button
+        variant="contained"
+        size="large"
+        fullWidth
+        className={classes.button}
+        onClick={handleGoogleLogin}
+      >
+        Login com Google
+      </Button>
+    </Box>
+  );
 }
+
+export default Login;
